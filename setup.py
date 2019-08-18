36be8c915b11ebc8b7d6bd1d9b82eb79f636d14d
Biswanath Saha <biswanathamz@gmail.com>
2019-08-18 21:22:52 +0530

    ...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="affnine_deltaleaf",
-    version="0.1",
+    version="0.2",
     author="Biswanath Saha",
     author_email="biswanathamz@gmail.com",
     description="Easy way to create MAP (Undirected Graph)and find shortest path from one Node to another.",
